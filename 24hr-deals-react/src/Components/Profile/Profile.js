b7e04b35d160c36830210bd00cf6d0fc32e1d985
Robert Basson <robertba@bbd.co.za>
2019-08-01 10:22:45 +0200

    Initial profile

---
+++ 24hr-deals-react/src/Components/Profile/Profile.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import ProfileImage from './ProfileImage';
+
+export default class Profile extends Component {
+    render() {
+        return (
+            <div>
+                <ProfileImage/>
+                <div>
+                    Name: odjidjfkpdjf
+                </div>
+                <div>
+                    Surname: dvfbfvf
+                </div>
+                <div>
+                    Email: kfmdkvmkfv
+                </div>
+            </div>
+        )
+    }
+}
---
+++ 24hr-deals-react/src/Components/Profile/ProfileImage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './ProfileImage.css';
+
+class ProfileImage extends React.Component {
+
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(
+      <div className ="Background"></div>
+    );
+  }
+}
+export default ProfileImage;

4c4b2eb36989ede32c5fa23db350c0c2a8d6a06c
Leonard van Gend <leonardv@bbd.co.za>
2019-08-01 13:11:12 +0200

    Added a stub route for getting products by deal, and added full routes for adding, and removing deals from transactions.

--- 24hr-deals-api/routes/transactionSpecific.routes.js
+++ 24hr-deals-api/routes/transactionSpecific.routes.js
@@ -1,14 +1,14 @@
-rootFactory = (app, dataBaseConnector) => {
+transactionRoots = (app, dataBaseConnector) => {
     const TransactionController = require(`../controllers/transaction.controller.js`);
 
     const controller = new TransactionController(dataBaseConnector);
 
-    app.post(`/transaction/addDeal/:transactionId`, (req, res) => {
+    app.post(`/transaction/:transactionId/addDeal`, (req, res) => {
         controller.addDeal(req, res);
     });
-    app.delete(`/transaction/removeDeal/:transactionId`, (req, res) => {
+    app.delete(`/transaction/:transactionId/removeDeal`, (req, res) => {
         controller.removeDeal(req, res);
     });
 }
 
-module.exports = rootFactory;
+module.exports = transactionRoots;
--- 24hr-deals-api/server.js
+++ 24hr-deals-api/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const rootFactory = require('./routes/routeFactory.routes');
-const DatabaseConnection = require('./database/database.connection')
+const DatabaseConnection = require('./database/database.connection');
+const transactionSpecificRoutes = require('./routes/transactionSpecific.routes');
+const dealSpecificRoutes = require('./routes/dealSpecific.routes');
 
 const port = 3000;
 
@@ -19,6 +21,9 @@ rootFactory(app, DatabaseConnection, "transaction");
 rootFactory(app, DatabaseConnection, "product");
 rootFactory(app, DatabaseConnection, "category");
 rootFactory(app, DatabaseConnection, "deal");
+transactionSpecificRoutes(app, DatabaseConnection);
+dealSpecificRoutes(app, DatabaseConnection);
+
 
 app.listen(port, () => {
     console.log(`Server is listening on port: ${port}`);
